684d33acb5a1a08039ab84cf47989a44ffcb13fc
Sadekrezkalla <sadekrezkalla111@gmail.com>
2025-06-25 17:02:53 +0300

    searchAPI

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: MovieDetails.routeName,
+      initialRoute: Home.routeName,
 
       routes: {
         Onboarding.routeName: (context) => Onboarding(),
