5905057488cdbe35ec739c9d90665a25388e295e
Sadekrezkalla <sadekrezkalla111@gmail.com>
2025-06-26 18:02:01 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: Home.routeName,
+      initialRoute: Auth.routeName,
       onGenerateRoute: (settings) {
         if (settings.name == MovieDetails.routeName) {
           final movie = settings.arguments as Movie;
